8a03888d21486fba9eaa86aea7b34201533f0631
Truc Ton <heodev@MacBook-Air-cua-abc.local>
2025-08-12 14:39:08 +0930

    Fix import path for Room

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
-import { Room } from "./room";
+import { Room } from "./Room";
 import React from "react";
 import HeroLights from "./HeroLights";
 import Particles from "./Particles";
